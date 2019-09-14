941208f1c7806041c94890990d3c81a1fd4bece0
Carlos Inga <carloseip192@gmail.com>
2019-09-14 11:45:38 -0500

    nuevo cambio

--- lib/models/card_color_model.dart
+++ lib/models/card_color_model.dart
@@ -3,5 +3,6 @@ import 'package:meta/meta.dart';
 class CardColorModel {
   bool isSelected;
   final int cardColor;
+  final int color;
   CardColorModel({@required this.isSelected, @required this.cardColor});
 }
